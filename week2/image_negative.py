ad0b221ffc2329b66217bfbc825b30c5601f0d84
itachi176 <lhoang17062000@gmail.com>
2021-09-17 20:19:49 +0700

    negative image

--- week2/image_negative.py
+++ week2/image_negative.py
@@ -1,1 +1,9 @@
 import cv2 
+img = cv2.imread('lenna.jpg', 0)
+img = cv2.resize(img, (640, 480))
+print(img.shape)
+# for i in range(48
+negative_img = 255 - img
+cv2.imshow("img", img)
+cv2.imshow("negative_img", negative_img)
+cv2.waitKey()
